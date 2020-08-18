5fb5fb49503380669ff382070edfe7b9cf1c29ee
daaniejelle <63784313+daaniejelle@users.noreply.github.com>
2020-08-18 20:51:22 +0200

    Login

--- src/main/java/com/example/restservice/Model/AppUser.java
+++ src/main/java/com/example/restservice/Model/AppUser.java
@@ -2,47 +2,47 @@ package com.example.restservice.Model;
 
 public class AppUser {
 
-private Long userId;
-private String userName;
-private String encrytedPassword;
+    private Long userId;
+    private String userName;
+    private String encrytedPassword;
 
-public AppUser() {
+    public AppUser() {
 
-}
+    }
 
-public AppUser(Long userId, String userName, String encrytedPassword) {
-this.userId = userId;
-this.userName = userName;
-this.encrytedPassword = encrytedPassword;
-}
+    public AppUser(Long userId, String userName, String encrytedPassword) {
+        this.userId = userId;
+        this.userName = userName;
+        this.encrytedPassword = encrytedPassword;
+    }
 
-public Long getUserId() {
-return userId;
-}
+    public Long getUserId() {
+        return userId;
+    }
 
-public void setUserId(Long userId) {
-this.userId = userId;
-}
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 
-public String getUserName() {
-return userName;
-}
+    public String getUserName() {
+        return userName;
+    }
 
-public void setUserName(String userName) {
-this.userName = userName;
-}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-public String getEncrytedPassword() {
-return encrytedPassword;
-}
+    public String getEncrytedPassword() {
+        return encrytedPassword;
+    }
 
-public void setEncrytedPassword(String encrytedPassword) {
-this.encrytedPassword = encrytedPassword;
-}
+    public void setEncrytedPassword(String encrytedPassword) {
+        this.encrytedPassword = encrytedPassword;
+    }
 
-@Override
-public String toString() {
-return this.userName + "/" + this.encrytedPassword;
-}
+    @Override
+    public String toString() {
+        return this.userName + "/" + this.encrytedPassword;
+    }
 
 }
--- src/main/java/com/example/restservice/mapper/AppUserMapper.java
+++ src/main/java/com/example/restservice/mapper/AppUserMapper.java
@@ -1,24 +1,24 @@
-//package com.example.restservice.mapper;
-//
-//import com.example.restservice.Model.AppUser;
-//import org.springframework.jdbc.core.RowMapper;
-//
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//
-//public class AppUserMapper implements RowMapper<AppUser> {
-//
-//    public static final String BASE_SQL //
-//            = "Select u.User_Id, u.User_Name, u.Encryted_Password From App_User u ";
-//
-//    @Override
-//    public AppUser mapRow(ResultSet rs, int rowNum) throws SQLException {
-//
-//        Long userId = rs.getLong("User_Id");
-//        String userName = rs.getString("User_Name");
-//        String encrytedPassword = rs.getString("Encryted_Password");
-//
-//        return new AppUser(userId, userName, encrytedPassword);
-//    }
-//
-//}
+package com.example.restservice.mapper;
+
+import com.example.restservice.Model.AppUser;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AppUserMapper implements RowMapper<AppUser> {
+
+    public static final String BASE_SQL //
+            = "Select u.User_Id, u.User_Name, u.Encryted_Password From App_User u ";
+
+    @Override
+    public AppUser mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+        Long userId = rs.getLong("User_Id");
+        String userName = rs.getString("User_Name");
+        String encrytedPassword = rs.getString("Encryted_Password");
+
+        return new AppUser(userId, userName, encrytedPassword);
+    }
+
+}
